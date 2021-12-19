bf2e6de3a6a2d26e1c0859c378b63054e20dfc17
varun-02 <varuna.it.21@nitj.ac.in>
2021-12-20 01:39:34 +0530

    Splash Screen

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private val splashScreen=2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(intent)
+            val intent = Intent(this@SplashActivity,main::class.java)
+            startActivity(intent)
+            finish()
+        }, splashScreen.toLong())
     }
 }
